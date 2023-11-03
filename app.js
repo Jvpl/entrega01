ad5413f9f263fe9922e73c34e7ab8d647f4bc2ef
Jvpl <jvpl1322@gmail.com>
2023-11-02 21:34:55 -0300

    test

---
+++ app.js
@@ -0,0 +1,4 @@
+let primeiroNumero = parseInt(prompt('Escolha um número'));
+let segundoNumero = parseInt(prompt('Escolha outro número'));
+let diferença = Math.abs(primeiroNumero - segundoNumero);
+console.log(diferença);
